57ab09a82dcc9663314ac41fbbac9f752c4ab44e
Oliver Zheng <gitcommits@oliverzheng.com>
2018-09-17 20:40:28 -0700

    Show Chrome extension `page_action` on github.com

--- src/background.js
+++ src/background.js
@@ -1,5 +1,23 @@
 // @flow
 // @format
 
+import {type Chrome} from './chrome';
+
 // eslint-disable-next-line no-console
 console.log("I'm a background script");
+
+declare var chrome: Chrome;
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {hostSuffix: 'github.com'},
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
+    ]);
+  });
+});
